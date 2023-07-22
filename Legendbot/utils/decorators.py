168d6e6bbd6ef860090fefdf12278cac6f41a97d
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-22 16:12:04 +0530

    Update decorators.py

--- Legendbot/utils/decorators.py
+++ Legendbot/utils/decorators.py
@@ -157,8 +157,8 @@ def errors_handler(func):
             result = output[0] + output[1]
             ftext += result
             pastelink = await paste_message(ftext)
-            text = "**LegendBot Error report**\n\n"
-            link = "[here](https://t.me/LegendBot_OP)"
+            text = "**RadhaUserBot Error report**\n\n"
+            link = "[here](https://t.me/RadhaX2Support)"
             text += "If you wanna you can report it"
             text += f"Just forward this message {link}.\n"
             text += "Nothing is logged except the fact of error and date\n\n"
