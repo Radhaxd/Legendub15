ac7474de9b84b22a103a3002c881aba5c9f2f984
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-23 18:59:54 +0530

    Update __init__.py

--- Legendbot/assistant/__init__.py
+++ Legendbot/assistant/__init__.py
@@ -6,4 +6,4 @@ from ..core.inlinebot import *
 
 LEGEND_USER = legend.me.first_name
 The_LegendBoy = legend.uid
-legend_mention = f"[{RADHA_USER}](tg://user?id={GHOSTRADHA})"
+legend_mention = f"[{LEGEND_USER}](tg://user?id={GHOSTRADHA})"
