b7e463639d57019faab90dd2a2c7ddd4c7d7e47f
Radha xD <133753917+Radhak8@users.noreply.github.com>
2023-07-23 10:52:59 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -3,16 +3,16 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 25769451
-    API_HASH = ""
+    APP_ID = 10940339
+    API_HASH = "4cdbbcc7a57756127b1f3df13a7ee175"
     # the name to display in your alive message
     ALIVE_NAME = "RadhaUserBot"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = ""
     # After cloning the repo and installing requirements do python3 telesetup.py an fill th this
-    LEGEND_STRING = ""
+    LEGEND_STRING = "1AZWarzYBu1I7L5wVNc_ShdZBulrEMlXTg9gXRZAMA7M4gZsslG2it1dvnvNf2sS9-cXTyuEuaCAG-STLcGg--6BzXzbT0fo2xUfiSAg-XtEvtXkL4CDytFCNAQqrN7uw2FuezdXXwn-Sl5I5HI9TaLBjvNN1e_T5elJt4M741wXvgRyvv3vVMGpr8Tvh6nBz9-KxEiVZ_PQrx8tfGJV17K4uH9aprpeGN4n3uMulmgj6PSmYzKZ56RWoOjK3fCgPfqhl3G7xjlKAqF-7ToqB3j23fnhLJtSaCNO708ntl1lGO5eIhN2_Hoonc2LiyTcQVt8vh4RMBCYE1bL39t9qm-1O8f4u7Jg="
     # create a new bot in @botfather and fill the following vales with bottoken
-    BOT_TOKEN = ""
+    BOT_TOKEN = "6012629686:AAHOfgIMs6jvRk360SxLy8jylDQQIJxFA5c"
     # command handler
     HANDLER = "."
     # command hanler for sudo
